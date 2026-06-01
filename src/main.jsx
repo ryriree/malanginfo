bc59da7702ca0b9b82fd234b9dc962bdc852bace
Ryri <ryriree@student.ub.ac.id>
2026-06-01 22:13:22 +0700

    ubah

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import '@fontsource/plus-jakarta-sans/600.css'
 import '@fontsource/plus-jakarta-sans/700.css'
 import '@fontsource/plus-jakarta-sans/800.css'
 import "./index.css"; 
-import App from './MalaNginfo.jsx'
+import App from './MalaNginfo.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
